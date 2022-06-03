b017a001b840759bd3d7722ea9fbddcecc8484b0
AmandaWalden <94139170+AmandaWalden@users.noreply.github.com>
2022-06-03 11:00:24 -0500

    Added functions to Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
-    return <div className="Dictionary">Hello</div>;
+    let [keyword , setKeyword] = useState("");
+    
+    function Search(event) {
+        event.preventDefault();
+        alert(`Searching for ${keyword} definition`);
+    }
+    
+    function handleKeywordChange(event) {
+        setKeyword(event.target.value);
+        
+    }
+    
+    return <div className="Dictionary">
+        <form onSubmit={Search}>
+            <input type="search" onChange={handleKeywordChange} />
+        </form>
+        </div>;
 }
