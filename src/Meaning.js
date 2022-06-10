8496628bdd767f6a1e7e1285cb718e49c5e4ec7f
AmandaWalden <94139170+AmandaWalden@users.noreply.github.com>
2022-06-10 00:28:57 -0500

    Updated the styling and defined the meanings.

--- src/Meaning.js
+++ src/Meaning.js
@@ -5,17 +5,10 @@ export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-        {props.meaning.definitions.map(function (definition, index) {
-        return (
-          <div key={index}>
-            <p>
-              {definition.definition}
+       <p>{props.meaning.definitions[0].definition}
               <br />
-              <em>{definition.example}</em>
+              <em>{props.meaning.definitions[0].example}</em>
             </p>
-          </div>
-        );
-      })}
     </div>
   );
 }
