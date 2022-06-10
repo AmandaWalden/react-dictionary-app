bb2de74bbcbc16a1df31f8d07da29d9473f9fc41
AmandaWalden <94139170+AmandaWalden@users.noreply.github.com>
2022-06-10 11:50:10 -0500

    Added Phonetics.js

--- src/Meaning.js
+++ src/Meaning.js
@@ -2,7 +2,6 @@ import React from "react";
 import Synonyms from "./Synonyms"
 
 export default function Meaning(props) {
-  console.log(props.meaning);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
@@ -14,9 +13,8 @@ export default function Meaning(props) {
               <strong>Definition</strong> : {definition.definition}
               <br />
               <strong>Example</strong> : <em>{definition.example}</em>
-              <br />
-              <Synonyms synonyms={definition.synonyms} />
             </p>
+            <Synonyms synonyms={definition.synonyms} />
           </div>
         );
       })}
